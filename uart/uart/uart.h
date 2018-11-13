96e884f580e91aba66b827cb90b033ef91bf50ba
Peter Goddard <PeterWIlliamGoddard@gmail.com>
2018-11-13 21:34:21 +0000

    UART library module.

---
+++ uart/uart/uart.h
@@ -0,0 +1,24 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 12/11/2018 15:20:33
+ *  Author: PeterGoddard
+ */ 
+
+
+#ifndef UART_H_
+#define UART_H_
+
+typedef enum
+{
+	UART_SUCCESS,
+	UART_FAILURE	
+}
+uart_status_enum_t;
+
+uart_status_enum_t uart_initialisation(void);
+void uart_enable_tx(void);
+void uart_disable_tx(void);
+void uart_transmit(uint8_t * data, size_t length);
+
+#endif /* UART_H_ */
